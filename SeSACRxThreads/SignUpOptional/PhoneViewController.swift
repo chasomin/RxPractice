de62ce756cb8da3ba265d985c5366c03097f3b5c
chasomin <somin49@naver.com>
2024-03-29 00:49:10 +0900

    PhoneNumber 화면 구현

--- SeSACRxThreads/SignUpOptional/PhoneViewController.swift
+++ SeSACRxThreads/SignUpOptional/PhoneViewController.swift
@@ -7,11 +7,18 @@
  
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class PhoneViewController: UIViewController {
    
     let phoneTextField = SignTextField(placeholderText: "연락처를 입력해주세요")
     let nextButton = PointButton(title: "다음")
+    let descriptionLabel = UILabel()
+    
+    let vaildText = Observable.just("10자 이상, 숫자만 적어주세요")
+    let commonNumber = Observable.just("010")
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,21 +27,58 @@ class PhoneViewController: UIViewController {
         
         configureLayout()
         
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        bind()
     }
     
-    @objc func nextButtonClicked() {
-        navigationController?.pushViewController(NicknameViewController(), animated: true)
+    private func bind() {
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                owner.navigationController?.pushViewController(NicknameViewController(), animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+        vaildText
+            .bind(to: descriptionLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        commonNumber
+            .bind(to: phoneTextField.rx.text)
+            .disposed(by: disposeBag)
+        
+        let numberCheck = phoneTextField.rx.text.orEmpty.map { Int($0) != nil }
+        let lengthCheck = phoneTextField.rx.text.orEmpty.map { $0.count >= 10 }
+        
+        let result = Observable.combineLatest(numberCheck, lengthCheck) { num, length in
+            return num && length
+        }
+        
+        result
+            .bind(to: nextButton.rx.isEnabled, descriptionLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        result
+            .bind(with: self) { owner, value in
+                let color = value ? UIColor.systemPink : UIColor.gray
+                owner.nextButton.backgroundColor = color
+            }
+            .disposed(by: disposeBag)
     }
 
     
     func configureLayout() {
         view.addSubview(phoneTextField)
         view.addSubview(nextButton)
-         
+        view.addSubview(descriptionLabel)
+        
         phoneTextField.snp.makeConstraints { make in
             make.height.equalTo(50)
             make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.height.equalTo(20)
+            make.top.equalTo(phoneTextField.snp.bottom)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
         
