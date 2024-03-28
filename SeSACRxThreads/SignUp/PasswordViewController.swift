d8e9cee6afdd2e870ccdfc59b14917c022b97eab
chasomin <somin49@naver.com>
2024-03-29 00:18:48 +0900

    Initial Commit

---
+++ SeSACRxThreads/SignUp/PasswordViewController.swift
@@ -0,0 +1,85 @@
+//
+//  PasswordViewController.swift
+//  SeSACRxThreads
+//
+//  Created by jack on 2023/10/30.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+class PasswordViewController: UIViewController {
+   
+    let passwordTextField = SignTextField(placeholderText: "비밀번호를 입력해주세요")
+    let nextButton = PointButton(title: "다음")
+    let descriptionLabel = UILabel()
+    
+    let vaildText = Observable.just("8자 이상 입력해주세요")
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = Color.white
+        
+        configureLayout()
+        
+        bind()
+         
+    }
+    
+    private func bind() {
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                owner.navigationController?.pushViewController(PhoneViewController(), animated: true)
+                print("Show Alert")
+            }
+            .disposed(by: disposeBag)
+        
+        vaildText
+            .bind(to: descriptionLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        let vaildation = passwordTextField.rx.text.orEmpty
+            .map { $0.count >= 8 }
+        
+        vaildation
+            .bind(to: nextButton.rx.isEnabled, descriptionLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        vaildation
+            .bind(with: self) { owner, value in
+                let color = value ? UIColor.systemPink : UIColor.gray
+                owner.nextButton.backgroundColor = color
+            }
+            .disposed(by: disposeBag)
+    }
+
+    func configureLayout() {
+        view.addSubview(passwordTextField)
+        view.addSubview(nextButton)
+        view.addSubview(descriptionLabel)
+         
+        passwordTextField.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.height.equalTo(20)
+            make.top.equalTo(passwordTextField.snp.bottom)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        nextButton.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(passwordTextField.snp.bottom).offset(30)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+    }
+
+}
