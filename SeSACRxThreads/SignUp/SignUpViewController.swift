2c1851d5281f6f47dd936515cb5462148477a636
chasomin <somin49@naver.com>
2024-03-31 04:41:22 +0900

    SignUpVC 이메일 구현

--- SeSACRxThreads/SignUp/SignUpViewController.swift
+++ SeSACRxThreads/SignUp/SignUpViewController.swift
@@ -7,12 +7,18 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class SignUpViewController: UIViewController {
 
     let emailTextField = SignTextField(placeholderText: "이메일을 입력해주세요")
     let validationButton = UIButton()
     let nextButton = PointButton(title: "다음")
+    
+    let userEmail = ["a@a.com", "b@b.com"]
+    let disposeBag = DisposeBag()
+    let availableEmail = BehaviorSubject(value: false)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +27,48 @@ class SignUpViewController: UIViewController {
         
         configureLayout()
         configure()
-        
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
-
+        bind()
     }
     
-    @objc func nextButtonClicked() {
-        navigationController?.pushViewController(PasswordViewController(), animated: true)
+    private func bind() {
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                owner.navigationController?.pushViewController(PasswordViewController(), animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+        let userEmailValid = emailTextField.rx.text.orEmpty
+            .map { $0.count >= 7 }
+        userEmailValid
+            .bind(with: self) { owner, value in
+                let text = value ? "중복 확인 해주세요" : "7자 이상 입력해주세요"
+                owner.nextButton.setTitle(text, for: .normal)
+            }
+            .disposed(by: disposeBag)
+        
+        availableEmail
+            .bind(with: self) { owner, value in
+                let text = value ? "사용 가능한 이메일" : "사용 불가능한 이메일"
+                owner.nextButton.setTitle(text, for: .normal)
+                let color = value ? UIColor.systemBlue : UIColor.gray
+                owner.nextButton.backgroundColor = color
+            }
+            .disposed(by: disposeBag)
+        
+        availableEmail
+            .bind(to: nextButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        validationButton.rx.tap
+            .bind(with: self) { owner, _ in
+                owner.availableEmail.onNext(!owner.userEmail.contains(owner.emailTextField.text!))
+            }
+            .disposed(by: disposeBag)
+        
+        let everythingValid = Observable.combineLatest(userEmailValid, availableEmail) { $0 && $1 }
+        
     }
-
+        
     func configure() {
         validationButton.setTitle("중복확인", for: .normal)
         validationButton.setTitleColor(Color.black, for: .normal)
